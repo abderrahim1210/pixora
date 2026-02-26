beb2aee0901034eeb3aac2d3f95ca2f879079016
Abderrahim Khali Ali <abderrahimkhaliali@gmail.com>
2026-02-26 22:30:33 +0000

    Reorganize and clean up the CSS style code by splitting it into several independent files and calling theme in main.scss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-import "./assets/css/App.scss";
+// import "./assets/css/App.scss";
+import "./assets/styles/main.scss";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 // import 'react-modal-sheet/dist/index.css'
