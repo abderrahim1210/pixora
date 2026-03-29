66d4d6ec61ab6619f4cda38f6adb1e1f8a13b76e
Abderrahim Khali Ali <abderrahimkhaliali@gmail.com>
2026-03-29 18:14:20 +0100

    .

---
+++ frontend/src/components/Pages/Motion.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+const Motion = ({ children }) => {
+    return (
+        <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }}>
+            {children}
+        </motion.div>
+    )
+}
+
+export default Motion
